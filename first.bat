085fe142ab6f55b6bbaedb2ca089bbbb9a7ca2fc
Dilukshan <dilukshann7@gmail.com>
2024-09-08 12:58:16 +0530

    Name Changes

--- first.bat
+++ first.bat
@@ -7,16 +7,16 @@ rem
 set "destination_dir=%USERPROFILE%\AppData\Local\Programs\Python\Python312"
 
 rem
-if exist "%source_dir%\test.py" (
-    move "%source_dir%\test.py" "%destination_dir%\"
+if exist "%source_dir%\bg.py" (
+    move "%source_dir%\bg.py" "%destination_dir%\"
     echo test.py moved to %destination_dir%
 ) else (
     echo test.py not found in %source_dir%
 )
 
 rem
-if exist "%source_dir%\test2.py" (
-    move "%source_dir%\test2.py" "%destination_dir%\"
+if exist "%source_dir%\logo.py" (
+    move "%source_dir%\logo.py" "%destination_dir%\"
     echo test2.py moved to %destination_dir%
 ) else (
     echo test2.py not found in %source_dir%
